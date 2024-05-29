6d2bbfe471afca57fb32c07312a28f4a4f84da23
ddohyeong <dohyeong7026@gmail.com>
2024-05-29 11:13:48 +0900

    Feat: 실시간 주식 데이터 조회 기능 구현

--- src/main/java/tmt/realtimechartservice/chart/presentation/KisController.java
+++ src/main/java/tmt/realtimechartservice/chart/presentation/KisController.java
@@ -1,19 +1,27 @@
 package tmt.realtimechartservice.chart.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import tmt.realtimechartservice.chart.service.KisSocketService;
+import tmt.realtimechartservice.chart.service.RedisPubSubServiceImp;
 
 @RestController
 @RequiredArgsConstructor
 public class KisController {
 	private final KisSocketService kisSocketService;
-
+	private final RedisPubSubServiceImp redisPubSubServiceImp;
 	@GetMapping("/real-time")
-	public Mono<Void> kis() {
+	public Mono<Void> connectKisWebSocketStockPrice() {
 		return kisSocketService.sendMessageToWebSocketServer();
 	}
 
+	@GetMapping(value = "/stream/{stockCode}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<String> stream(@PathVariable("stockCode") String stockCode){
+		return redisPubSubServiceImp.getMessage(stockCode);
+	}
 }
