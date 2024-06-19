dfa95f16388b02c7e4955c146ea07b13b444e7e8
ddohyeong <dohyeong7026@gmail.com>
2024-06-19 20:45:57 +0900

    fix: 스케줄링 위한 어노테이션 추가 및 불필요한 web gradle 삭제

--- src/main/java/tmt/realtimechartservice/RealTimeChartServiceApplication.java
+++ src/main/java/tmt/realtimechartservice/RealTimeChartServiceApplication.java
@@ -2,7 +2,9 @@ package tmt.realtimechartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class RealTimeChartServiceApplication {
 
