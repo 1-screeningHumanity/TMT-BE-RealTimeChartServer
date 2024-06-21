b9e9b61c8ac5db27074a6289726e2f333f6b6051
ddohyeong <dohyeong7026@gmail.com>
2024-06-21 10:50:10 +0900

    fix: 실시간 데이터 처리 중 서버가 클라이언트의 상태 변화를 감지하는 로직 추가

--- src/main/java/tmt/realtimechartservice/chart/service/RedisPubSubServiceImp.java
+++ src/main/java/tmt/realtimechartservice/chart/service/RedisPubSubServiceImp.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.ReactiveRedisMessageListenerContainer;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 
 @Slf4j
@@ -29,23 +31,34 @@ public class RedisPubSubServiceImp implements RedisPubSubService {
 	}
 
 	@Override
-	public Flux<String> getRealTimePrice(String stockCode) {
+	public Flux<String> getRealTimePrice(String stockCode, ServerWebExchange exchange) {
 		return reactiveRedisService.getPrice(stockCode)
 				.concatWith(sink.asFlux()
 						.filter(message -> message.startsWith("stock:" + stockCode))
-						.mergeWith(Flux.interval(Duration.ofSeconds(10)).map(tick -> "keep-alive"))
-						.doOnError(error -> log.warn("Error in getRealTimePrice stream: {}", error.getMessage(), error))  // 에러 발생 시 로그 출력
-						.doOnCancel(() -> log.info("Price Client disconnected"))); // 클라이언트 연결 해제 시 로그 출력)
+						.mergeWith(Flux.interval(Duration.ofSeconds(10))
+								.map(tick -> "keep-alive")
+								.onBackpressureDrop()
+						)
+						.takeUntilOther(exchange.getResponse().writeWith(Mono.never()))
+						.doOnError(error -> log.warn("Error in getRealTimePrice stream: {}",
+								error.getMessage(), error))  // 에러 발생 시 로그 출력
+						.doOnCancel(() -> log.info(
+								"Price Client disconnected"))); // 클라이언트 연결 해제 시 로그 출력)
 
 	}
 
 	@Override
-	public Flux<String> getAskPrice(String stockCode) {
+	public Flux<String> getAskPrice(String stockCode,ServerWebExchange exchange) {
 		return reactiveRedisService.getAskPrice(stockCode)
 				.concatWith(sink.asFlux()
 						.filter(message -> message.startsWith("stock:askPrice-" + stockCode))
-						.mergeWith(Flux.interval(Duration.ofSeconds(10)).map(tick -> "keep-alive"))
-						.doOnError(error -> log.warn("Error in getAskPrice stream: {}", error.getMessage(), error))  // 에러 발생 시 로그 출력
-						.doOnCancel(() -> log.info("Ask Price Client disconnected"))); // 클라이언트 연결 해제 시 로그 출력)
+						.mergeWith(Flux.interval(Duration.ofSeconds(10))
+								.map(tick -> "keep-alive")
+								.onBackpressureDrop())
+						.takeUntilOther(exchange.getResponse().writeWith(Mono.never()))
+						.doOnError(error -> log.warn("Error in getAskPrice stream: {}",
+								error.getMessage(), error))  // 에러 발생 시 로그 출력
+						.doOnCancel(() -> log.info(
+								"Ask Price Client disconnected"))); // 클라이언트 연결 해제 시 로그 출력)
 	}
 }
