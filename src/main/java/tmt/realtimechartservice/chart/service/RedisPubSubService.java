c91cfe0cefe51e62c7efb027aecdf8c50f61929b
ddohyeong <dohyeong7026@gmail.com>
2024-06-27 14:35:32 +0900

    refactor: 실시간 조회 이슈 해결

--- src/main/java/tmt/realtimechartservice/chart/service/RedisPubSubService.java
+++ src/main/java/tmt/realtimechartservice/chart/service/RedisPubSubService.java
@@ -1,14 +1,13 @@
 package tmt.realtimechartservice.chart.service;
 
-import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 
 public interface RedisPubSubService {
 //	Flux<String> getRealTimePrice(String stockCode);
 
-	Flux<String> getRealTimePrice(String stockCode, ServerWebExchange exchange);
+	Flux<String> getRealTimePrice(String stockCode);
 
 //	Flux<String> getAskPrice(String stockCode);
 
-	Flux<String> getAskPrice(String stockCode, ServerWebExchange exchange);
+	Flux<String> getAskPrice(String stockCode);
 }
